01f6020b21649c64d759c9fc3d6e2b1da905158f
suichanwa <augustinnegura746@gmail.com>
2022-05-29 10:36:39 +0300

    oop study

--- oop/__main__.py
+++ oop/__main__.py
@@ -1,3 +1,31 @@
+class mainClass():
+    name = 'name from mainClass'
+
+
+class anoth:
+    def __init__(self, name):
+        self.name = name
+        self.age = 23
+        self.email = 'someemail'
+
+        @property
+        def age(self):
+            return self.age
+        
+        @age.setter
+        def age(self, age):
+            if age < 0:
+                raise ValueError("Age must be positive")
+            self.age = age
+    
+class anthh(anoth):
+    #change email name from anoth class
+    def __init__(self, name):
+        super().__init__(name)
+        self.email = 'is she changed?'
+
+
+
 class Example:
     def __init__(self):
         print('testng')
@@ -20,16 +48,67 @@ exa = Example()
 exa.say_something()
 exa.returnnum()
 
-#show persons data
 person = Persons("John", "25")
 print(person)
 
 
-#test code with pytest
 class testing:
-    def test_one(self):
-        x = "this"
-        assert "a" in x
+    def testing_AnotherPerson(name):
+        name = "John"
+        assert person.name == name
+        assert person.age == 1
+        person.display()
 
-pytest = testing()
-pytest.test_one()
+class atributes(mainClass):
+    type = 'something'
+    des = 'another'
+    default_name = 'somename'
+
+    def __init__(self, name):
+        if name:
+            self.name = name
+        else:
+            self.name = atributes.default_name
+    @staticmethod
+    def static_method():
+        print(atributes.name)
+
+class obj:
+    def __init__(self, name ,age):
+        self.name = name
+        self.age = age
+    
+    def display(self):
+        print(self.name, self.age)
+    
+    def __str__(self):
+        return "Name: {0}, Age: {1}".format(self.name, self.age)
+
+class codeWars:
+    
+    arr = [1,2,3,4,5,6,7,8,9,10]
+
+    def sum_array(arr):
+        sum = 0
+        for i in arr:
+            sum += i
+        return sum
+
+    def greeting(name):
+        return "Hello, {0} how are you doing today?".format(name)
+
+    #We need a function that counts the number of sheep present in the array
+    def count_sheep(sheep):
+        return sum([1 for s in sheep if s == True])
+
+
+
+if __name__ == '__main__':
+    enter = anthh("name")
+    enter.age = -1
+
+    print(enter.email)
+    classExample = atributes("name")
+    classExample.static_method()
+    obc = obj("name", 3)
+    obc.display()
