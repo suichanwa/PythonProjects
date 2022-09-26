8e184ddd55be9bf7b9d2607c71f52f1ebee9850a
suichanwa <augustinnegura746@gmail.com>
2022-09-26 19:51:59 +0300

    лшмн

--- jsonapp/gui.py
+++ jsonapp/gui.py
@@ -1,11 +1,30 @@
-import PySimpleGUI as sg
-import os.path
+import kivy
+from kivy.uix.gridlayout import GridLayout
+from kivy.app import App
+from kivy.uix.label import Label
 
+class MainApp(App):
+    def build(self):
+        label = Label(
+                    text='Hello from Kivy',
+                    size_hint=(.5, .5),
+                    pos_hint={'center_x': .5, 'center_y': .5},
+                    font_size="34"
+                )
 
-image_viewer_column = [
-    [sg.Text("Choose an image from list on left:")],
-    [sg.Text(size=(40, 1), key="-TOUT-")],
-    [sg.Image(key="-IMAGE-")],
-]
+        return label
 
+class LoginScreen(GridLayout):
+    def __init__(self, **kwargs):
+        super(LoginScreen, self).__init__(**kwargs)
+        self.cols = 2
+        self.add_widget(self.username)
+        self.add_widget = (Label(texr='name'))
+        
+        
+    
 
+if __name__ == "__main__":
+    #app = MainApp()
+    #app.run()
+    pass
